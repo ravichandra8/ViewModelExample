f094bd7a6b2b989a4ba0839cb10558e8e3dc82c3
lenovo <ravichandrachakka8@gmail.com>
2017-12-22 13:13:51 +0530

    mvvm for textview and editext

--- app/src/main/java/com/example/ravichandra/viewmodelexample/MainActivity.java
+++ app/src/main/java/com/example/ravichandra/viewmodelexample/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.ravichandra.viewmodelexample;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,17 +15,41 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
       TextView text;
     Button btn;
+     SampleViewModel model;
+    EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        model = ViewModelProviders.of(this).get(SampleViewModel.class);
         text=(TextView)findViewById(R.id.text);
+        edit=(EditText)findViewById(R.id.edit);
+        edit.setText(model.getEditstr());
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            model.setEditstr(editable.toString());
+
+            }
+        });
         btn=(Button)findViewById(R.id.btn);
-        SampleViewModel model = ViewModelProviders.of(this).get(SampleViewModel.class);
+
+        text.setText(model.getCount()+"");
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SampleViewModel.setCount(SampleViewModel.getCount() + 1);
+                model.setCount(model.getCount()+1);
+                text.setText(model.getCount()+"");
             }
         });
     }
--- app/src/main/java/com/example/ravichandra/viewmodelexample/SampleViewModel.java
+++ app/src/main/java/com/example/ravichandra/viewmodelexample/SampleViewModel.java
@@ -9,6 +9,15 @@ import android.arch.lifecycle.ViewModel;
 public class SampleViewModel extends ViewModel {
 
     private int count;
+    private String editstr;
+
+    public String getEditstr() {
+        return editstr;
+    }
+
+    public void setEditstr(String editstr) {
+        this.editstr = editstr;
+    }
 
     public void setCount(int count) {
         this.count = count;
